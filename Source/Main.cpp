b92e9f3f3ee7136382ed18b6c6608addbc9ad6d3
student3 <student3@TIC220411>
2026-09-11 09:59:06 +0900

    main説明書き中

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,17 @@
 #include "DxLib.h"
 
 
+/*
+* ゲームループ詳細
+* gitに関して
+* ブランチ説明詳細
+* 
+* 一欄目が
+* 二欄目がひと段落
+* 三欄目が大きい変更
+* ここら辺苦手	
+*/
+
 /// <summary>
 /// メイン
 /// </summary>
@@ -12,11 +23,15 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
+	// DXライブラリ前初期化
+
 	// DXライブラリ初期化処理
 	if (DxLib::DxLib_Init() == -1)
 	{
 		return -1;		// エラーが起きたら直ちに終了
 	}
+
+	// DXライブラリ後
 
 	// DXライブラリ使用の終了処理
 	DxLib::DxLib_End();
